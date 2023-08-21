b2404ad5aab473d774934e14841f9283bd9e489d
Rojoin <nachoarrastua@gmail.com>
2023-08-21 17:39:53 -0300

    Add basic Kitchen scene

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Serialization;
@@ -7,14 +9,12 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Vector2ChannelSO OnMoveChannel;
     [SerializeField] private float speed;
-    [FormerlySerializedAs("playerSize")] [SerializeField]
-    private float radius = 0.7f;
-    private Coroutine movement;
+    [SerializeField] private float radius = 0.7f;
     [SerializeField] private float height;
+    private Coroutine movement;
+    [SerializeField] private Mesh rendererMesh;
+    [SerializeField] private bool activateGizmos;
 
-    private void Awake()
-    {
-    }
 
     private void OnEnable()
     {
@@ -28,7 +28,6 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move(Vector2 dir)
     {
-        Debug.Log(dir);
         if (movement != null)
         {
             StopCoroutine(movement);
@@ -43,20 +42,29 @@ public class PlayerMovement : MonoBehaviour
         {
             Vector3 moveDir = new Vector3(dir.x, 0, dir.y);
             float time = Time.deltaTime;
-            
+
             bool canMove = CanMove(dir, time, ref moveDir);
-            
+
             if (canMove)
             {
-                transform.position += moveDir * time * speed;
+                transform.position += moveDir * (time * speed);
             }
 
             yield return null;
         }
     }
 
+    /// <summary>
+    /// Checks if the player is able to move towards the input direction.
+    /// Returns true if the player can move at least in one direction. 
+    /// </summary>
+    /// <param name="dir">InputDirection.</param>
+    /// <param name="time">Delta Time.</param>
+    /// <param name="moveDir">Direction to be return. It changes depending on the available direction.</param>
+    /// <returns></returns>
     private bool CanMove(Vector2 dir, float time, ref Vector3 moveDir)
     {
+        moveDir = new Vector3(dir.x, 0, dir.y);
         bool canMove = !IsColliding(moveDir, time);
 
         if (!canMove)
@@ -81,9 +89,47 @@ public class PlayerMovement : MonoBehaviour
         return canMove;
     }
 
+    /// <summary>
+    /// Returns true if the player is collinding.
+    /// </summary>
+    /// <param name="moveDir">Direction of the player</param>
+    /// <param name="time">Delta Time</param>
+    /// <returns></returns>
     private bool IsColliding(Vector3 moveDir, float time)
     {
         var position = transform.position;
         return Physics.CapsuleCast(position, position + Vector3.up * height, radius, moveDir, speed * time);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (!activateGizmos) return;
+        DrawWireCapsule(transform.position,transform.rotation,radius,height,Color.red);
+    }
+
+    private static void DrawWireCapsule(Vector3 _pos, Quaternion _rot, float _radius, float _height, Color _color = default(Color))
+    {
+        if (_color != default(Color))
+            Handles.color = _color;
+        Matrix4x4 angleMatrix = Matrix4x4.TRS(_pos, _rot, Handles.matrix.lossyScale);
+        using (new Handles.DrawingScope(angleMatrix))
+        {
+            var pointOffset = (_height - (_radius * 2)) / 2;
+
+            //draw sideways
+            Handles.DrawWireArc(Vector3.up * pointOffset, Vector3.left, Vector3.back, -180, _radius);
+            Handles.DrawLine(new Vector3(0, pointOffset, -_radius), new Vector3(0, -pointOffset, -_radius));
+            Handles.DrawLine(new Vector3(0, pointOffset, _radius), new Vector3(0, -pointOffset, _radius));
+            Handles.DrawWireArc(Vector3.down * pointOffset, Vector3.left, Vector3.back, 180, _radius);
+            //draw frontways
+            Handles.DrawWireArc(Vector3.up * pointOffset, Vector3.back, Vector3.left, 180, _radius);
+            Handles.DrawLine(new Vector3(-_radius, pointOffset, 0), new Vector3(-_radius, -pointOffset, 0));
+            Handles.DrawLine(new Vector3(_radius, pointOffset, 0), new Vector3(_radius, -pointOffset, 0));
+            Handles.DrawWireArc(Vector3.down * pointOffset, Vector3.back, Vector3.left, -180, _radius);
+            //draw center
+            Handles.DrawWireDisc(Vector3.up * pointOffset, Vector3.up, _radius);
+            Handles.DrawWireDisc(Vector3.down * pointOffset, Vector3.up, _radius);
+
+        }
     }
 }
