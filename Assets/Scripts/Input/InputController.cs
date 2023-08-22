0cfc0399b92d316d8eaff34e3b2f22ec79f151e8
Rojoin <nachoarrastua@gmail.com>
2023-08-22 00:04:09 -0300

    Add CollisionGizmos

--- Assets/Scripts/Input/InputController.cs
+++ Assets/Scripts/Input/InputController.cs
@@ -1,45 +1,45 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Serialization;
 
-public class InputController : MonoBehaviour
+namespace GameInputs
 {
-    [Header("Inputs")]
-    [SerializeField]private Vector2ChannelSO OnMoveChannel;
-    [SerializeField]private VoidChannelSO OnRollChannel;
-    [SerializeField]private VoidChannelSO OnPauseChannel;
-    [SerializeField]private BoolChannelSO OnActionChannel;
-  
+    public class InputController : MonoBehaviour
+    {
+        [Header("Inputs")]
+        [SerializeField]private Vector2ChannelSO OnMoveChannel;
+        [SerializeField]private VoidChannelSO OnRollChannel;
+        [SerializeField]private VoidChannelSO OnPauseChannel;
+        [SerializeField]private BoolChannelSO OnActionChannel;
+        
+        public void OnMove(InputAction.CallbackContext ctx)
+        {
+            OnMoveChannel.RaiseEvent(ctx.ReadValue<Vector2>());
+        }
+        public void OnRollInput(InputAction.CallbackContext ctx)
+        {
+            if (ctx.performed)
+            {
+                OnRollChannel.RaiseEvent();
+            }
+        }
+        public void OnAction(InputAction.CallbackContext ctx)
+        {
+            if (ctx.performed)
+            {
+                OnActionChannel.RaiseEvent(true);
+            }
+            else if (ctx.canceled)
+            {
+                OnActionChannel.RaiseEvent(false);
+            }
+        }
+        public void OnPauseMode(InputAction.CallbackContext ctx)
+        {
+            if (ctx.performed)
+            {
+                OnPauseChannel.RaiseEvent();
+            }
+        }
 
-    public void OnMove(InputAction.CallbackContext ctx)
-    {
-        OnMoveChannel.RaiseEvent(ctx.ReadValue<Vector2>());
     }
-    public void OnRollInput(InputAction.CallbackContext ctx)
-    {
-        if (ctx.performed)
-        {
-            OnRollChannel.RaiseEvent();
-        }
-    }
-    public void OnAction(InputAction.CallbackContext ctx)
-    {
-        if (ctx.performed)
-        {
-            OnActionChannel.RaiseEvent(true);
-        }
-        else if (ctx.canceled)
-        {
-            OnActionChannel.RaiseEvent(false);
-        }
-    }
-    public void OnPauseMode(InputAction.CallbackContext ctx)
-    {
-        if (ctx.performed)
-        {
-            OnPauseChannel.RaiseEvent();
-        }
-    }
-
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,135 +1,137 @@
-using System;
 using System.Collections;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.Serialization;
 
-public class PlayerMovement : MonoBehaviour
+namespace Player
 {
-    [SerializeField] private Vector2ChannelSO OnMoveChannel;
-    [SerializeField] private float speed;
-    [SerializeField] private float radius = 0.7f;
-    [SerializeField] private float height;
-    private Coroutine movement;
-    [SerializeField] private Mesh rendererMesh;
-    [SerializeField] private bool activateGizmos;
-
-
-    private void OnEnable()
+    public class PlayerMovement : MonoBehaviour
     {
-        OnMoveChannel.Subscribe(Move);
-    }
+        [SerializeField] private Vector2ChannelSO OnMoveChannel;
+        [SerializeField] private float speed;
+        [SerializeField] private float radius = 0.7f;
+        [SerializeField] private float height;
+        private Coroutine movement;
+        [SerializeField] private bool activateGizmos;
 
-    private void OnDisable()
-    {
-        OnMoveChannel.Unsubscribe(Move);
-    }
 
-    private void Move(Vector2 dir)
-    {
-        if (movement != null)
+        private void OnEnable()
         {
-            StopCoroutine(movement);
+            OnMoveChannel.Subscribe(Move);
         }
 
-        movement = StartCoroutine(Movement(dir));
-    }
-
-    private IEnumerator Movement(Vector2 dir)
-    {
-        while (dir != Vector2.zero)
+        private void OnDisable()
         {
-            Vector3 moveDir = new Vector3(dir.x, 0, dir.y);
-            float time = Time.deltaTime;
-
-            bool canMove = CanMove(dir, time, ref moveDir);
-
-            if (canMove)
-            {
-                transform.position += moveDir * (time * speed);
-            }
-
-            yield return null;
+            OnMoveChannel.Unsubscribe(Move);
         }
-    }
 
-    /// <summary>
-    /// Checks if the player is able to move towards the input direction.
-    /// Returns true if the player can move at least in one direction. 
-    /// </summary>
-    /// <param name="dir">InputDirection.</param>
-    /// <param name="time">Delta Time.</param>
-    /// <param name="moveDir">Direction to be return. It changes depending on the available direction.</param>
-    /// <returns></returns>
-    private bool CanMove(Vector2 dir, float time, ref Vector3 moveDir)
-    {
-        moveDir = new Vector3(dir.x, 0, dir.y);
-        bool canMove = !IsColliding(moveDir, time);
-
-        if (!canMove)
+        private void Move(Vector2 dir)
         {
-            Vector3 moveDirX = new Vector3(dir.x, 0, 0);
-            canMove = !IsColliding(moveDirX, time);
-            if (canMove)
+            if (movement != null)
             {
-                moveDir = moveDirX;
+                StopCoroutine(movement);
             }
-            else
+
+            movement = StartCoroutine(Movement(dir));
+        }
+
+        /// <summary>
+        /// Movement Corroutine
+        /// </summary>
+        /// <param name="dir"></param>
+        /// <returns></returns>
+        private IEnumerator Movement(Vector2 dir)
+        {
+            while (dir != Vector2.zero)
             {
-                Vector3 moveDirY = new Vector3(0, 0, dir.y);
-                canMove = !IsColliding(moveDirY, time);
+                Vector3 moveDir = new Vector3(dir.x, 0, dir.y);
+                float time = Time.deltaTime;
+                bool canMove = CanMove(dir, time, ref moveDir);
                 if (canMove)
                 {
-                    moveDir = moveDirY;
+                    transform.position += moveDir * (time * speed);
                 }
+
+                yield return null;
             }
         }
 
-        return canMove;
-    }
-
-    /// <summary>
-    /// Returns true if the player is collinding.
-    /// </summary>
-    /// <param name="moveDir">Direction of the player</param>
-    /// <param name="time">Delta Time</param>
-    /// <returns></returns>
-    private bool IsColliding(Vector3 moveDir, float time)
-    {
-        var position = transform.position;
-        return Physics.CapsuleCast(position, position + Vector3.up * height, radius, moveDir, speed * time);
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        if (!activateGizmos) return;
-        DrawWireCapsule(transform.position,transform.rotation,radius,height,Color.red);
-    }
-
-    private static void DrawWireCapsule(Vector3 _pos, Quaternion _rot, float _radius, float _height, Color _color = default(Color))
-    {
-        if (_color != default(Color))
-            Handles.color = _color;
-        Matrix4x4 angleMatrix = Matrix4x4.TRS(_pos, _rot, Handles.matrix.lossyScale);
-        using (new Handles.DrawingScope(angleMatrix))
+        /// <summary>
+        /// Checks if the player is able to move towards the input direction.
+        /// Returns true if the player can move at least in one direction. 
+        /// </summary>
+        /// <param name="dir">InputDirection.</param>
+        /// <param name="time">Delta Time.</param>
+        /// <param name="moveDir">Direction to be return. It changes depending on the available direction.</param>
+        /// <returns></returns>
+        private bool CanMove(Vector2 dir, float time, ref Vector3 moveDir)
         {
-            var pointOffset = (_height - (_radius * 2)) / 2;
+            moveDir = new Vector3(dir.x, 0, dir.y);
+            bool canMove = !IsColliding(moveDir, time);
 
-            //draw sideways
-            Handles.DrawWireArc(Vector3.up * pointOffset, Vector3.left, Vector3.back, -180, _radius);
-            Handles.DrawLine(new Vector3(0, pointOffset, -_radius), new Vector3(0, -pointOffset, -_radius));
-            Handles.DrawLine(new Vector3(0, pointOffset, _radius), new Vector3(0, -pointOffset, _radius));
-            Handles.DrawWireArc(Vector3.down * pointOffset, Vector3.left, Vector3.back, 180, _radius);
-            //draw frontways
-            Handles.DrawWireArc(Vector3.up * pointOffset, Vector3.back, Vector3.left, 180, _radius);
-            Handles.DrawLine(new Vector3(-_radius, pointOffset, 0), new Vector3(-_radius, -pointOffset, 0));
-            Handles.DrawLine(new Vector3(_radius, pointOffset, 0), new Vector3(_radius, -pointOffset, 0));
-            Handles.DrawWireArc(Vector3.down * pointOffset, Vector3.back, Vector3.left, -180, _radius);
-            //draw center
-            Handles.DrawWireDisc(Vector3.up * pointOffset, Vector3.up, _radius);
-            Handles.DrawWireDisc(Vector3.down * pointOffset, Vector3.up, _radius);
+            if (!canMove)
+            {
+                Vector3 moveDirX = new Vector3(dir.x, 0, 0);
+                canMove = !IsColliding(moveDirX, time);
+                if (canMove)
+                {
+                    moveDir = moveDirX;
+                }
+                else
+                {
+                    Vector3 moveDirY = new Vector3(0, 0, dir.y);
+                    canMove = !IsColliding(moveDirY, time);
+                    if (canMove)
+                    {
+                        moveDir = moveDirY;
+                    }
+                }
+            }
 
+            return canMove;
+        }
+
+        /// <summary>
+        /// Returns true if the player is collinding.
+        /// </summary>
+        /// <param name="moveDir">Direction of the player</param>
+        /// <param name="time">Delta Time</param>
+        /// <returns></returns>
+        private bool IsColliding(Vector3 moveDir, float time)
+        {
+            var position = transform.position;
+            return Physics.CapsuleCast(position, position + Vector3.up * height, radius, moveDir, speed * time);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (!activateGizmos) return;
+            DrawWireCapsule(transform.position, transform.rotation, radius, height, Color.red);
+        }
+
+        private static void DrawWireCapsule(Vector3 _pos, Quaternion _rot, float _radius, float _height,
+            Color _color = default(Color))
+        {
+            if (_color != default(Color))
+                Handles.color = _color;
+            Matrix4x4 angleMatrix = Matrix4x4.TRS(_pos, _rot, Handles.matrix.lossyScale);
+            using (new Handles.DrawingScope(angleMatrix))
+            {
+                var pointOffset = (_height - (_radius * 2)) / 2;
+
+                //draw sideways
+                Handles.DrawWireArc(Vector3.up * pointOffset, Vector3.left, Vector3.back, -180, _radius);
+                Handles.DrawLine(new Vector3(0, pointOffset, -_radius), new Vector3(0, -pointOffset, -_radius));
+                Handles.DrawLine(new Vector3(0, pointOffset, _radius), new Vector3(0, -pointOffset, _radius));
+                Handles.DrawWireArc(Vector3.down * pointOffset, Vector3.left, Vector3.back, 180, _radius);
+                //draw frontways
+                Handles.DrawWireArc(Vector3.up * pointOffset, Vector3.back, Vector3.left, 180, _radius);
+                Handles.DrawLine(new Vector3(-_radius, pointOffset, 0), new Vector3(-_radius, -pointOffset, 0));
+                Handles.DrawLine(new Vector3(_radius, pointOffset, 0), new Vector3(_radius, -pointOffset, 0));
+                Handles.DrawWireArc(Vector3.down * pointOffset, Vector3.back, Vector3.left, -180, _radius);
+                //draw center
+                Handles.DrawWireDisc(Vector3.up * pointOffset, Vector3.up, _radius);
+                Handles.DrawWireDisc(Vector3.down * pointOffset, Vector3.up, _radius);
+            }
         }
     }
 }
