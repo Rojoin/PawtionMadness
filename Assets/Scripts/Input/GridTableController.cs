65cc6f105401d860ce4f51583afe454dd02bfbb6
Rojoin <nachoarrastua@gmail.com>
2023-08-26 17:11:52 -0300

    Add GridTableController

---
+++ Assets/Scripts/Input/GridTableController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Grid;
+using UnityEngine;
+
+namespace Table
+{
+    public class GridTableController : Table
+    {
+        [SerializeField] private VoidChannelSO ChangeToGridChannel;
+        public override void OnInteraction()
+        {
+            ChangeToGridChannel.RaiseEvent();
+        }
+    }
+}
