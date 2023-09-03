7a173c71554c5159896eb453fa6159dcdac873b8
Rojoin <nachoarrastua@gmail.com>
2023-09-03 18:24:35 -0300

    Add ClearDesk class

---
+++ Assets/Scripts/Table/ClearDesk.cs
@@ -0,0 +1,26 @@
+﻿using Item;
+using UnityEngine;
+
+namespace Table
+{
+    public class ClearDesk : Table
+    {
+        private Pickable _pickable;
+        [SerializeField] private Transform _itemPos;
+
+        public override void OnInteraction(PlayerInventory playerInventory = null)
+        {
+            if (!_pickable && playerInventory.hasPickable())
+            {
+                _pickable = Instantiate(playerInventory.GetPickable(), _itemPos.position, Quaternion.identity,
+                    _itemPos);
+                playerInventory.DestroyPickable();
+            }
+            else if (_pickable && !playerInventory.hasPickable())
+            {
+                playerInventory.SetPickable(_pickable);
+                Destroy(_pickable.gameObject);
+            }
+        }
+    }
+}
