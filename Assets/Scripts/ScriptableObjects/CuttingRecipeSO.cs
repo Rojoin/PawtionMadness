4a1e0ac20e7d6d69b0585f0ba4a83f5c2bfa5c40
Rojoin <nachoarrastua@gmail.com>
2023-09-10 23:16:28 -0300

    Add CuttingRecipeSO

---
+++ Assets/Scripts/ScriptableObjects/CuttingRecipeSO.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Create CuttingRecipeSO", fileName = "CuttingRecipeSO", order = 0)]
+public class CuttingRecipeSO : ScriptableObject
+{
+    public KitchenObjectSO inputIngredient;
+    public KitchenObjectSO processedIngredient;
+}
