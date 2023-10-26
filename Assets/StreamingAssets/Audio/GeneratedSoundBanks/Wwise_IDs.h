d0423e0e3688b1199b516c8e7731e32d134edc02
feli1906 <felimolina1906@gmail.com>
2023-10-26 22:56:24 +0200

    AMB+PickUp

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,7 +14,19 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID MAINMENUMUSIC = 1991862712U;
+        static const AkUniqueID PLAY_AGARRAR_ITEM_FRASCO = 2559549617U;
+        static const AkUniqueID PLAY_AGARRAR_ITEM_PORCION = 2191355065U;
+        static const AkUniqueID PLAY_AMB_BOSQUE_NOCHE = 1452442514U;
+        static const AkUniqueID PLAY_AMB_PAJAROS_CUERVOS = 2278612795U;
+        static const AkUniqueID PLAY_CALDERO = 4083340090U;
+        static const AkUniqueID PLAY_DESPLAZAMIENTO_ENEMIGO_ALAS = 2839911833U;
+        static const AkUniqueID PLAY_DISPARO = 213559042U;
         static const AkUniqueID PLAY_INGAMEMUSIC = 3799629626U;
+        static const AkUniqueID PLAY_LIBRO_ABRIR_3 = 707032169U;
+        static const AkUniqueID PLAY_OLEADA = 417218340U;
+        static const AkUniqueID PLAY_OLEADA_FINAL = 3691145017U;
+        static const AkUniqueID PLAY_POCION_PREPARANDOSE = 4052632251U;
+        static const AkUniqueID PLAY_PONER_ITEM_EN_CALDERO = 2748973737U;
     } // namespace EVENTS
 
     namespace BANKS
