6cdc354257018c2bc1f9b6a07a1b99515af2cddc
Rojoin <nachoarrastua@gmail.com>
2023-11-07 20:17:28 -0300

    Add new Credits

---
+++ Assets/Scripts/Menu/ScrollFix.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Menu
+{
+    public class ScrollFix : ScrollRect {
+        public override void OnDrag(PointerEventData eventData)
+        {
+            base.OnDrag(eventData);
+
+            OnBeginDrag(eventData);
+        }
+    }
+}
