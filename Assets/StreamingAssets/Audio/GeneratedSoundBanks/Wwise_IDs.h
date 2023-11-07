6f59d7b706adbded5ceb483dfa026b344e89eebe
feli1906 <felimolina1906@gmail.com>
2023-11-07 13:42:32 +0100

    Sfx in audio-merge branch wwise. Tutorial and MainMenu events.
    Sfx added inside this branch on wwise (actor mixer). Events separation between MainMenu and Tutorial.

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -17,10 +17,7 @@ namespace AK
         static const AkUniqueID PLAY_AGARRAR_ITEM_FRASCO = 2559549617U;
         static const AkUniqueID PLAY_AGARRAR_ITEM_PORCION = 2191355065U;
         static const AkUniqueID PLAY_AMB_BOSQUE_NOCHE = 1452442514U;
-        static const AkUniqueID PLAY_AMB_PAJAROS_CUERVOS = 2278612795U;
         static const AkUniqueID PLAY_CALDERO = 4083340090U;
-        static const AkUniqueID PLAY_DESPLAZAMIENTO_ENEMIGO_ALAS = 2839911833U;
-        static const AkUniqueID PLAY_DISPARO = 213559042U;
         static const AkUniqueID PLAY_INGAMEMUSIC = 3799629626U;
         static const AkUniqueID PLAY_LIBRO_ABRIR_3 = 707032169U;
         static const AkUniqueID PLAY_OLEADA = 417218340U;
@@ -33,6 +30,7 @@ namespace AK
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID INGAME = 984691642U;
         static const AkUniqueID MAINMENU = 3604647259U;
         static const AkUniqueID TUTORIAL = 3762955427U;
     } // namespace BANKS
