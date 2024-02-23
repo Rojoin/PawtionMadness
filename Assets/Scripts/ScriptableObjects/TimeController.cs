213b3efd602cedc15dc8e13d69bdb329f4eea365
Rojoin <nachoarrastua@gmail.com>
2024-02-23 19:27:52 -0300

    Add SlowTIme when using the grid

--- Assets/Scripts/ScriptableObjects/TimeController.cs
+++ Assets/Scripts/ScriptableObjects/TimeController.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 
+[CreateAssetMenu(menuName = "Create TimeController", fileName = "TimeController", order = 0)]
 public class TimeController : ScriptableObject
 {
-    public const float defaultTimeScale = 1.0f;
+    public float defaultTimeScale = 1.0f;
     [Range(0.25f,1.0f)]
     public float slowTimeScale = 0.5f;
 }
