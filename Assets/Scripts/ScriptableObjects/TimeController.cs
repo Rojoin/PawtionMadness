147d4640d0c8a7eae5b0269b5be861a85115907e
Rojoin <nachoarrastua@gmail.com>
2024-02-23 19:27:09 -0300

    Add Time Controller SO

---
+++ Assets/Scripts/ScriptableObjects/TimeController.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class TimeController : ScriptableObject
+{
+    public const float defaultTimeScale = 1.0f;
+    [Range(0.25f,1.0f)]
+    public float slowTimeScale = 0.5f;
+}
